a5cac1c2d482f69a5b3fa8ac70f33948b1f36564
Romero Ezequiel <romerorezequiel@gmail.com>
2022-01-03 15:27:48 -0300

    Ejemplo de como se consume un Web Service Online

---
+++ ConsumirWSOnline/ConsumirWSOnline/Default.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ConsumirWSOnline.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+            Num1 <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            <br />
+
+            Num2 <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+            <br />
+
+            <asp:Button ID="Button1" runat="server" Text="Suma" OnClick="Button1_Click" /><br />  
+            <asp:Button ID="Button2" runat="server" Text="Resta" OnClick="Button2_Click"  /><br /> 
+            <asp:Button ID="Button3" runat="server" Text="Multiplicacion" OnClick="Button3_Click" /><br /> 
+            <asp:Button ID="Button4" runat="server" Text="Division" OnClick="Button4_Click"  /><br />
+
+            <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ ConsumirWSOnline/ConsumirWSOnline/Default.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ConsumirWSOnline
+{
+    public partial class Default : System.Web.UI.Page
+    {
+
+        WSOnline.CalculatorSoapClient WS = new WSOnline.CalculatorSoapClient();
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            //ACA OCUPO EL METODO DE SUMA DE SOAP y para visualizarlo en la etiqueta hay que parsearlo a string
+            Label1.Text = WS.Add(Convert.ToInt32(TextBox1.Text), Convert.ToInt32(TextBox2.Text)).ToString();
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            Label1.Text = WS.Subtract(Convert.ToInt32(TextBox1.Text), Convert.ToInt32(TextBox2.Text)).ToString();
+
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            Label1.Text = WS.Multiply(Convert.ToInt32(TextBox1.Text), Convert.ToInt32(TextBox2.Text)).ToString();
+
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            Label1.Text = WS.Divide(Convert.ToInt32(TextBox1.Text), Convert.ToInt32(TextBox2.Text)).ToString();
+
+        }
+    }
+}
